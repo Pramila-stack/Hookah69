1e07bea0d3da8e9d139cbc639a48e2bc1284e5da
Pramila Tamang <pramilatmg.np@gmail.com>
2026-06-26 15:49:33 +0545

    Updated

--- hookah69/settings.py
+++ hookah69/settings.py
@@ -130,13 +130,8 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "core" / "static",
-]
 
-STATICFILES_STORAGE = (
-    "whitenoise.storage.CompressedManifestStaticFilesStorage"
-)
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
